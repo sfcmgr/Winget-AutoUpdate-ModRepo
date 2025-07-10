ac7e31686291cba1f4402bc5f64dc159de1a3a0b
sfcmgr <shearingc@colchsfc.ac.uk>
2025-07-10 08:29:38 +0100

    updated git installed to use template

--- Git.Git-installed.ps1
+++ Git.Git-installed.ps1
@@ -1,6 +1,72 @@
-$appName = 'Git'
-$desktopShortcut = Get-ChildItem -Path 'C:\Users\Public\Desktop' | Where-Object { ($_.Name -replace '\s', '') -match $appName } 
-if ($desktopShortcut) {
-    Remove-Item  -Path $desktopShortcut.FullName -Force -ErrorAction SilentlyContinue
+<# A mods template for apps
+Possible use cases:
+"$Mods\AppID-preinstall.ps1"
+"$Mods\AppID-install.ps1"
+"$Mods\AppID-upgrade.ps1"
+"$Mods\AppID-installed.ps1"
+"$Mods\AppID-notinstalled.ps1"
+#>
+
+<# FUNCTIONS #>
+. $PSScriptRoot\_Mods-Functions.ps1
+
+# Beginning of Desktop Link Name to Remove - optional wildcard (*) after, without .lnk, multiple: "lnk1*","lnk2"
+$Lnk = @("Git")
+
+# Remove file/directory, multiple: "file1","file2" Example:
+# $DelFile = @("${env:ProgramFiles}\PowerToys\PowerToys.Update.exe")
+$DelFile = @("${env:ProgramData}\Microsoft\Windows\Start Menu\Programs\Git")
+
+
+<# MAIN #>
+if ($RunSystem) {
+    Invoke-ModsApp $RunSystem $RunSwitch $RunWait ""
 }
-Remove-Item -Path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Git" -Recurse -Force -ErrorAction SilentlyContinue
+if ($Proc) {
+    Stop-ModsProc $Proc
+}
+if ($Svc) {
+    Stop-ModsSvc $Svc
+}
+if ($Wait) {
+    Wait-ModsProc $Wait
+}
+if ($WingetIDInst) {
+    Install-WingetID $WingetIDInst
+}
+if ($WingetIDUninst) {
+    Uninstall-WingetID $WingetIDUninst
+}
+if ($AppUninst) {
+    Uninstall-ModsApp $AppUninst $AllVersions
+}
+if ($Lnk) {
+    Remove-ModsLnk $Lnk
+}
+if ($AddKey -and $AddValue -and $AddTypeData -and $AddType) {
+    Add-ModsReg $AddKey $AddValue $AddTypeData $AddType
+}
+if ($DelKey) {
+    Remove-ModsReg $DelKey $DelValue
+}
+if ($DelFile) {
+    Remove-ModsFile $DelFile
+}
+if ($RenFile -and $NewName) {
+    Rename-ModsFile $RenFile $NewName
+}
+if ($CopyFile -and $CopyTo) {
+    Copy-ModsFile $CopyFile $CopyTo
+}
+if ($File -and $FindText -and $ReplaceText) {
+    Edit-ModsFile $File $FindText $ReplaceText
+}
+if ($GrantPath) {
+    Grant-ModsPath $GrantPath
+}
+if ($RunUser) {
+    Invoke-ModsApp $RunUser "" "" $User
+}
+
+
+<# EXTRAS #>
